fc62b7c7275e0872100b970f9e5aa848cc6d986a
j_venegas <josuesinvr1@gmail.com>
2022-06-09 20:28:14 -0500

    add form beta functionalities

--- sources/app.js
+++ sources/app.js
@@ -1,10 +1,12 @@
 let btn = document.querySelector('#publicar');
 let caCont = document.getElementById('card-grid');
+let btn2 = document.querySelector('#niggers');
 
-let titleInput = 'lel';
-let authorInput = 'lol';
+
 
 const newBook = () => {
+    let titleInput = document.getElementById('titulo').value;
+    let authorInput = document.getElementById('autor').value;
     const el = document.createElement('div');
     const img = document.createElement('div');
     const info = document.createElement('div');
@@ -30,3 +32,7 @@ const newBook = () => {
 
 btn.onclick = () => {
     newBook();}
+
+btn2.onclick = () => {
+    newBook();
+}
